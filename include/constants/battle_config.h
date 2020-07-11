7b8a1b50d9cf9724046a921f09ddbeb6f0abdc43
= <=>
2020-07-11 11:27:46 +0200

    Updade battle_config.h

--- include/constants/battle_config.h
+++ include/constants/battle_config.h
@@ -20,33 +20,6 @@
 #define SPECIES_WISHIWASHI 0
 #define SPECIES_WISHIWASHI_SCHOOL 10004
 
-// Items with peculiar battle effects. Remove them if they're properly placed in constant/items.h
-#define ITEM_GRISEOUS_ORB 0
-#define ITEM_OCCA_BERRY 204
-#define ITEM_PASSHO_BERRY 205
-#define ITEM_WACAN_BERRY 206
-#define ITEM_RINDO_BERRY 207
-#define ITEM_YACHE_BERRY 208
-#define ITEM_CHOPLE_BERRY 177
-#define ITEM_KEBIA_BERRY 178
-#define ITEM_SHUCA_BERRY 179
-#define ITEM_COBA_BERRY 180
-#define ITEM_PAYAPA_BERRY 181
-#define ITEM_TANGA_BERRY 182
-#define ITEM_CHARTI_BERRY 183
-#define ITEM_KASIB_BERRY 184
-#define ITEM_HABAN_BERRY 185
-#define ITEM_COLBUR_BERRY 186
-#define ITEM_BABIRI_BERRY 187
-#define ITEM_CHILAN_BERRY 188
-#define ITEM_ROSELI_BERRY 189
-#define ITEM_MICLE_BERRY 197
-#define ITEM_CUSTAP_BERRY 199
-#define ITEM_JABOCA_BERRY 200
-#define ITEM_ROWAP_BERRY 201
-#define ITEM_KEE_BERRY 202
-#define ITEM_MARANGA_BERRY 203
-
 #define GEN_3 0
 #define GEN_4 1
 #define GEN_5 2
@@ -97,24 +70,24 @@
 // Other
 #define B_FAST_INTRO                TRUE  // If set to TRUE, battle intro texts print at the same time as animation of a Pokémon, as opposing to waiting for the animation to end.
 #define B_SLEEP_TURNS               GEN_6 // In Gen5+, sleep lasts for 1-3 turns instead of 2-5 turns.
-#define B_PARALYZE_ELECTRIC         GEN_6 // In Gen6+, Electric type Pokémon can't be paralyzed.
-#define B_POWDER_GRASS              GEN_6 // In Gen6+, Grass type Pokémon are immune to powder and spore moves.
+#define B_PARALYZE_ELECTRIC         GEN_5 // In Gen6+, Electric type Pokémon can't be paralyzed.
+#define B_POWDER_GRASS              GEN_5 // In Gen6+, Grass type Pokémon are immune to powder and spore moves.
 
 // Animation Settings
-#define NEW_SWORD_PARTICLE              TRUE    // update swords dance particle
-#define NEW_LEECH_SEED_PARTICLE         TRUE    //update leech seed's animation particle
-#define NEW_HORN_ATTACK_PARTICLE        TRUE    //update horn attack's horn
-#define NEW_LEAF_PARTICLE               TRUE    // update leaf particle
-#define NEW_EMBER_PARTICLES             TRUE    //updates ember fire particle
-#define NEW_MEAN_LOOK_PARTICLE          TRUE    //update mean look eye
-#define NEW_TEETH_PARTICLE              TRUE    //update bite/crunch teeth particle
-#define NEW_HANDS_FEET_PARTICLE         TRUE    //update chop/kick/punch particles
-#define NEW_SPIKES_PARTICLE             TRUE    //update spikes particle
-#define NEW_FLY_BUBBLE_PARTICLE         TRUE    //update fly 'bubble' particle
-#define NEW_CURSE_NAIL_PARTICLE         TRUE    //updates curse nail
-#define NEW_BATON_PASS_BALL_PARTICLE    TRUE    //update baton pass pokeball sprite
-#define NEW_MORNING_SUN_STAR_PARTICLE   TRUE    //updates morning sun star particles
-#define NEW_IMPACT_PALETTE              TRUE    //updates the basic 'hit' particle
-#define NEW_SURF_PARTICLE_PALETTE       TRUE    //updates the surf wave palette
+#define NEW_SWORD_PARTICLE              FALSE    // update swords dance particle
+#define NEW_LEECH_SEED_PARTICLE         FALSE    //update leech seed's animation particle
+#define NEW_HORN_ATTACK_PARTICLE        FALSE    //update horn attack's horn
+#define NEW_LEAF_PARTICLE               FALSE    // update leaf particle
+#define NEW_EMBER_PARTICLES             FALSE    //updates ember fire particle
+#define NEW_MEAN_LOOK_PARTICLE          FALSE    //update mean look eye
+#define NEW_TEETH_PARTICLE              FALSE    //update bite/crunch teeth particle
+#define NEW_HANDS_FEET_PARTICLE         FALSE    //update chop/kick/punch particles
+#define NEW_SPIKES_PARTICLE             FALSE    //update spikes particle
+#define NEW_FLY_BUBBLE_PARTICLE         FALSE    //update fly 'bubble' particle
+#define NEW_CURSE_NAIL_PARTICLE         FALSE    //updates curse nail
+#define NEW_BATON_PASS_BALL_PARTICLE    FALSE    //update baton pass pokeball sprite
+#define NEW_MORNING_SUN_STAR_PARTICLE   FALSE    //updates morning sun star particles
+#define NEW_IMPACT_PALETTE              FALSE    //updates the basic 'hit' particle
+#define NEW_SURF_PARTICLE_PALETTE       FALSE    //updates the surf wave palette
 
 #endif // GUARD_CONSTANTS_BATTLE_CONFIG_H
